fce676cfb3a69f1aed754090b088ab0687e9ccfd
jiaqi <jiaqi0602@gmail.com>
2020-12-14 23:28:01 +0800

    maze generation

---
+++ maze_generation/maze_generation_unity/Assets/Scripts/MazeCell.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeCell
+{
+    public bool Visited = false;
+    public GameObject upWall;
+    public GameObject downWall;
+    public GameObject leftWall;
+    public GameObject rightWall;
+
+
+
+}
